e233af8f4722fab25e6197dc2593a66a30d0aadd
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-01-09 16:42:59 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const data = { name, email, password }
-        let res = await fetch('http://localhost:3000/api/signup', {
+        let res = await fetch(process.env.API_URI, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
